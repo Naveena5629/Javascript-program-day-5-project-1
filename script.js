655d3e9fc93d59c6ef5b306176756854f3cfb758
Naveena5629 <naveenabalasundari@gmail.com>
2024-07-09 16:19:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("off").addEventListener('click',function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2024/06/20/11/12/nature-8841864_1280.png"
+});
+    document.getElementById("on").addEventListener("click",function(){
+    document.getElementById("image").src="https://media.istockphoto.com/id/1450094329/photo/colorful-sunset-viewed-from-the-pink-sea-beach-with-soft-waves.jpg?s=612x612&w=0&k=20&c=_Eb1qwBuf2-D7wI5-jzwkHm5TwTzD5JYl5WxpM3nlLM=" 
+});
